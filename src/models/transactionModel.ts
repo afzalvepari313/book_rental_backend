5c25fac7b011b210732162b304228060f51a2249
afzal <afza313dz@gmail.com>
2024-09-08 17:31:27 +0530

    minor changes

--- src/models/transactionModel.ts
+++ src/models/transactionModel.ts
@@ -2,15 +2,17 @@ import mongoose, { Schema, Document } from 'mongoose';
 
 interface ITransaction extends Document {
   userId: Schema.Types.ObjectId;
-  bookId: Schema.Types.ObjectId;
+  bookName: string;
   issueDate: Date;
   returnDate?: Date;
+  rent?: number;
 }
 
 const transactionSchema = new Schema<ITransaction>({
   userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-  bookId: { type: Schema.Types.ObjectId, ref: 'Book', required: true },
-  issueDate: { type: Date, required: true },
+  bookName: { type: String, required: true },
+  rent: { type: Number},
+  issueDate: { type: Date},
   returnDate: { type: Date },
 });
 
--- src/routes/transactionRoutes.ts
+++ src/routes/transactionRoutes.ts
@@ -1,10 +1,20 @@
 import { Router } from 'express';
-import { issueBook, returnBook, getTransactionHistory } from '../controllers/transactionController';
+import {
+    issueBook,
+    returnBook,
+    getBookDetails,
+    getTotalRent,
+    getUserBooks,
+    getBooksByDateRange
+} from '../controllers/transactionController';
 
 const router = Router();
 
 router.post('/issue', issueBook);
 router.post('/return', returnBook);
-router.get('/history', getTransactionHistory);
+router.get('/book/:bookName', getBookDetails);
+router.get('/book/rent/:bookName', getTotalRent);
+router.get('/user/:userId', getUserBooks);
+router.get('/date-range', getBooksByDateRange);
 
 export default router;
